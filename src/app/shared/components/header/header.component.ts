da4b76c4004a15a82a5321f2a63cd059a6d98c07
discrimine <discrimine96@gmail.com>
2024-08-26 13:43:42 +0200

    pr comments

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,17 +2,11 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [
-		MatToolbarModule,
-		MatIconModule,
-		MatButtonModule,
-		MatDividerModule,
-	],
+	imports: [MatToolbarModule, MatIconModule, MatButtonModule],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 })
