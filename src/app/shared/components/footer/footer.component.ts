1eb048ed73d2591e4db27e17d16dfab3db84f142
1Artemida <darkhella2102@gmail.com>
2024-08-26 12:38:20 +0200

    linter fix

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss',
+	selector: 'app-footer',
+	standalone: true,
+	imports: [MatToolbarModule],
+	templateUrl: './footer.component.html',
+	styleUrl: './footer.component.scss',
 })
 export class FooterComponent {}
