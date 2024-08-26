378203045495cc4ec98d18d3e3a5a547ce6343de
1Artemida <darkhella2102@gmail.com>
2024-08-26 20:19:34 +0200

    Added styles and fixed lint

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [],
+	imports: [MatToolbarModule, MatIconModule, MatButtonModule],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 })
