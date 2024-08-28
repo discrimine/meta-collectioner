f14401431a8acb09e26bc0b6d5f2a7d75cac5dd1
1Artemida <darkhella2102@gmail.com>
2024-08-28 18:05:25 +0200

    linter fix

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -11,46 +11,46 @@ import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
-	selector: 'app-collection',
-	standalone: true,
-	imports: [
-		HttpClientModule,
-		MatInputModule,
-		MatIconModule,
-		MatCardModule,
-		MatProgressSpinnerModule,
-		CommonModule,
-	],
-	templateUrl: './collection.component.html',
-	styleUrl: './collection.component.scss',
+    selector: 'app-collection',
+    standalone: true,
+    imports: [
+        HttpClientModule,
+        MatInputModule,
+        MatIconModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        CommonModule,
+    ],
+    templateUrl: './collection.component.html',
+    styleUrl: './collection.component.scss',
 })
 export class CollectionComponent implements OnInit, OnDestroy {
-	public anime: AnimeEntity[] = [];
-	public isLoading: boolean = true;
+    public anime: AnimeEntity[] = [];
+    public isLoading: boolean = true;
 
-	private subscriptions = new Subscription();
+    private subscriptions = new Subscription();
 
-	constructor(
-		private animeService: AnimeService,
-		private activatedRoute: ActivatedRoute
-	) {}
+    constructor(
+        private animeService: AnimeService,
+        private activatedRoute: ActivatedRoute
+    ) {}
 
-	ngOnInit(): void {
-		this.subscriptions.add(
-			this.activatedRoute.params.subscribe(params => {
-				console.log(params);
-			})
-		);
+    ngOnInit(): void {
+        this.subscriptions.add(
+            this.activatedRoute.params.subscribe(params => {
+                console.log(params);
+            })
+        );
 
-		this.subscriptions.add(
-			this.animeService.getList().subscribe((anime: AnimeEntity[]) => {
-				this.anime = anime;
-				this.isLoading = false;
-			})
-		);
-	}
+        this.subscriptions.add(
+            this.animeService.getList().subscribe((anime: AnimeEntity[]) => {
+                this.anime = anime;
+                this.isLoading = false;
+            })
+        );
+    }
 
-	ngOnDestroy(): void {
-		this.subscriptions.unsubscribe();
-	}
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
+    }
 }
