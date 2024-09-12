b5b3bf6974c2c3be0ad83d6e283ba42a6032e44f
1Artemida <58728864+1Artemida@users.noreply.github.com>
2024-09-12 12:26:20 +0200

    linter fix (#7)
    * linter fix
    
    * deleted console.log
    
    * fixed comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { CollectionComponent } from './collections/collection/collection.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { CollectionsComponent } from './collections/collections.component';
 import { PreviewComponent } from './collections/shared/components/preview/preview.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { AuthGuard } from './auth/shared/helpers/auth.guard';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {
@@ -13,6 +17,7 @@ export const routes: Routes = [
     {
         path: 'collections',
         component: CollectionsComponent,
+        canActivate: [AuthGuard],
         children: [
             {
                 path: '',
@@ -28,4 +33,19 @@ export const routes: Routes = [
         path: 'about',
         component: AboutPageComponent,
     },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'registration',
+        component: RegistrationComponent,
+    },
+    { path: '**', redirectTo: '' },
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../auth/shared/services/auth.service';
+import { Router, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
+    imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterModule, CommonModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-    constructor() {}
+export class HeaderComponent implements OnInit, OnDestroy {
+    public isLoggedIn = false;
+    private authStatusSubscription: Subscription = new Subscription();
+
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
+
+    ngOnInit() {
+        this.authStatusSubscription.add(
+            this.authService.loggedUser.subscribe(user => {
+                this.isLoggedIn = !!user;
+            })
+        );
+    }
+
+    ngOnDestroy() {
+        this.authStatusSubscription.unsubscribe();
+    }
+
+    onLogout() {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+    }
 }
